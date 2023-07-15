16b33ac700aa507ed65000973b9530f9e25f9625
PythJavHacker <adam.eldesoky@gmail.com>
2023-07-15 20:29:21 +0300

    Initial commit

---
+++ Password.py
@@ -0,0 +1,20 @@
+import random
+
+
+def shuffle(string):
+    tempList = list(string)
+    random.shuffle(tempList)
+    return ''.join(tempList)
+
+
+special = ['?', '!', '.', '#']
+UpperLet = chr(random.randint(65, 90))
+UpperLet1 = chr(random.randint(65, 90))
+LowerLet = chr(random.randint(97, 122))
+LowerLet1 = chr(random.randint(97, 122))
+no1 = chr(random.randint(48, 57))
+no2 = chr(random.randint(48, 57))
+Special = (special[random.randint(0, 3)])
+Password = UpperLet + UpperLet1 + LowerLet + LowerLet1 + no2 + no1 + Special
+Password = shuffle(Password)
+print('Your password is: ', Password)
